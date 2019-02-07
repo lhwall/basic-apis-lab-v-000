e784eed30c94ff84a14ee8fbe9f1e228dafc565f
Lucy Wall <lhwall@gmail.com>
2019-02-07 18:18:15 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,39 @@
 class RepositoriesController < ApplicationController
 
   def search
+    binding.pry
+    begin
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = clientID
+    req.params['client_secret'] = clientSecret
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @repositories = body["response"]["repositories"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
 
+  end
+  render 'search'
   end
 
   def github_search
+    begin
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        # req.params['client_id'] = b1bb3f1be65d525b319c
+        # req.params['client_secret'] = 2b4748113a55766f2ff7bdae9b6cfc8b190feaff
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @repositories = body["response"]["repositories"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+
+
   end
+  render 'search'
+  end
+
 end
