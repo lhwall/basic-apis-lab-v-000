05c7055f699aa7936a9b2882aefd7f24d7713c39
Lucy Wall <lhwall@gmail.com>
2019-02-07 18:51:37 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,7 +4,7 @@ class RepositoriesController < ApplicationController
     binding.pry
     begin
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = clientID
+    req.params['client_id'] = clientID
     req.params['client_secret'] = clientSecret
     end
     body = JSON.parse(@resp.body)
